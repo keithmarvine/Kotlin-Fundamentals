6599dc31a19c451249762c6209ba5d7ad84c1131
Keith Marvin <138390683+keithmarvine@users.noreply.github.com>
2025-11-10 12:42:35 +0300

    Add files via upload

---
+++ introduction.kt
@@ -0,0 +1,8 @@
+fun main() {
+
+    println("Welcome to your first Kotlin programming class!")
+    println(100)
+    println(15+20)
+    print("eMobilis")
+
+}
---
+++ variables.kt
@@ -0,0 +1,25 @@
+//A variable is a container used to store a value
+
+fun main() {
+    var firstname = "Keith"
+    println (firstname)
+
+    var number = 10
+//    Re-assigning a value
+    number = 500
+    println(number)
+
+    var x = 35
+
+//    val is used for constant values eg
+
+    val pi = 3.142
+
+    println(pi)
+
+    var language: String
+
+    language = "Kotlin"
+    println(language)
+
+}
